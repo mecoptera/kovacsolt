48e573c1276754d5792728eb7895b75f33ca0a55
Mecoptera <krazyqwed@gmail.com>
2019-11-25 22:05:09 +0100

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,5 @@ class DatabaseSeeder extends Seeder {
   public function run() {
     $this->call(UsersTableSeeder::class);
     $this->call(AdminsTableSeeder::class);
-    $this->call(BaseProductsTableSeeder::class);
   }
 }
